383e00c855ea44d87077f7ab151f6e64455e9042
botflux <victor.mendele68@gmail.com>
2019-09-19 21:45:46 +0200

    :herb: refactor: add a success message for app happy path

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const fetch = require('node-fetch')
+const commandLineArgs = require('command-line-args')
 const login = require('./api/login')
 const badger = require('./api/badger')
-
-const commandLineArgs = require('command-line-args')
 const app = require('./src/app')
-const { commandLineOptionsDefinition } = require('./src/constants')
 
+const { commandLineOptionsDefinition } = require('./src/constants')
 const options = commandLineArgs(commandLineOptionsDefinition)
 
 const login = login (fetch) 
 const badger = badger (fetch)
 
 app(login, badger) (options)
-    .then (state => { console.log(state) })
+    .then (() => console.log('Successfully badged'))
     .catch (error => console.log(error))
