479aae0859ff405d7d8c8ed922c73f0e6f10938d
botflux <victor.mendele68@gmail.com>
2019-09-19 21:02:13 +0200

    :herb: refactor: use custom exception for request error

--- src/app.js
+++ src/app.js
@@ -1,21 +1,22 @@
 const login = require('./api/login')
 const badger = require('./api/badger')
 
+const BadgerFailedError = require ('./exception/badger-failed-error')
+const LoginFailedError = require ('./exception/login-failed-error')
+
 const app = fetch => async ({ presence, user, password, userId }) => {
 
     const credentials = { user, password }
     const loginResponse = await login (fetch) (credentials)
 
     if (!loginResponse.success) {
-        console.log('Login failed')
-        return 1
+        throw new LoginFailedError ('Login has failed')
     }
 
     const badgerResponse = await badger(fetch) ({ presence, token: loginResponse.token, userId })
 
     if (!badgerResponse.success) {
-        console.log('Badger failed')
-        return 2
+        throw new BadgerFailedError ('Badger has failed')
     }
 
     console.log(badgerResponse)
