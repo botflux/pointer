6596f13015260670b0829cb27208c2ed91d2025b
botflux <victor.mendele68@gmail.com>
2019-09-19 21:24:35 +0200

    :herb: refactor: use already hydrated login and badger function

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const fetch = require('node-fetch')
+const login = require('./api/login')
+const badger = require('./api/badger')
+
 const commandLineArgs = require('command-line-args')
 const app = require('./src/app')
 const { commandLineOptionsDefinition } = require('./src/constants')
 
 const options = commandLineArgs(commandLineOptionsDefinition)
 
-app(fetch) (options)
+app(login (fetch), badger (fetch)) (options)
     .then (state => { console.log(state) })
--- src/app.js
+++ src/app.js
@@ -1,19 +1,16 @@
-const login = require('./api/login')
-const badger = require('./api/badger')
-
 const BadgerFailedError = require ('./exception/badger-failed-error')
 const LoginFailedError = require ('./exception/login-failed-error')
 
-const app = fetch => async ({ presence, user, password, userId }) => {
+const app = (login, badger) => async ({ presence, user, password, userId }) => {
 
     const credentials = { user, password }
-    const loginResponse = await login (fetch) (credentials)
+    const loginResponse = await login (credentials)
 
     if (!loginResponse.success) {
         throw new LoginFailedError ('Login has failed')
     }
 
-    const badgerResponse = await badger(fetch) ({ presence, token: loginResponse.token, userId })
+    const badgerResponse = await badger ({ presence, token: loginResponse.token, userId })
 
     if (!badgerResponse.success) {
         throw new BadgerFailedError ('Badger has failed')
