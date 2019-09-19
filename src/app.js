90910343964b0b4011289a73ab01801ca624d633
botflux <victor.mendele68@gmail.com>
2019-09-19 21:39:31 +0200

    :pencil: docs: add comments for app function

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 const BadgerFailedError = require ('./exception/badger-failed-error')
 const LoginFailedError = require ('./exception/login-failed-error')
 
+/**
+ * Authenticate and badge a given user
+ * 
+ * @param {Function} login 
+ * @param {Function} badger 
+ */
 const app = (login, badger) => async ({ presence, user, password, userId }) => {
 
     const credentials = { user, password }
@@ -15,9 +21,6 @@ const app = (login, badger) => async ({ presence, user, password, userId }) => {
     if (!badgerResponse.success) {
         throw new BadgerFailedError ('Badger has failed')
     }
-
-    console.log(badgerResponse)
-    return 0
 }
 
 module.exports = app
