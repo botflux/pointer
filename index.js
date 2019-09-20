9d7ec4606866a68e79db8a5ec890fb1bfd7f9a98
Victor Mendele <victor.mendele68@gmail.com>
2019-09-20 15:37:31 +0200

    :hammer: fix: function names in index

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const fetch = require('node-fetch')
 const commandLineArgs = require('command-line-args')
-const login = require('./api/login')
-const badger = require('./api/badger')
+const makeLogin = require('./src/api/login')
+const makeBadger = require('./src/api/badger')
 const app = require('./src/app')
 
 const { commandLineOptionsDefinition } = require('./src/constants')
 const options = commandLineArgs(commandLineOptionsDefinition)
 
-const login = login (fetch) 
-const badger = badger (fetch)
+const login = makeLogin (fetch) 
+const badger = makeBadger (fetch)
 
 app(login, badger) (options)
     .then (() => console.log('Successfully badged'))
